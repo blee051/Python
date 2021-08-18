55ae43cfb8b47a1f1002c63ee977b40d6731dce4
blee051 <blee051@ucr.edu>
2021-08-18 07:57:08 -0700

    Update main.py

--- main.py
+++ main.py
@@ -30,6 +30,7 @@ def playerCommand(p, mmap, g, l):
     gameCol = 0
     correctInput = False
 
+    # Checking for correct input
     for element in g:
         for e in element:
             if e == coordinate:
@@ -51,53 +52,53 @@ def playerCommand(p, mmap, g, l):
 def checkConnectThree(m, lletter, pplayer):
     otherLetter = 'X'
     if lletter == 'X':
-        otherLetter = 'O'
+        otherLetter = 'O' # I forgot why i did this
     rowMatchAmmount = 0
     collumMatchAmmount = 0
-
+    # We are checking the rows and collums for connect 3's by counting the letter of the current player
     i=0
     r=0
     while r<3:
         while i<3:
             if m[i][r] == lletter:
-                rowMatchAmmount += 1
+                rowMatchAmmount += 1 # row count
             if m[r][i] == lletter:
-                collumMatchAmmount += 1
+                collumMatchAmmount += 1 # collum count
             i+=1
 
         if rowMatchAmmount == 3:
-            print("Player ",pplayer," WINS!")
+            print("Player ",pplayer," WINS!")  # WIN
             return True
         if collumMatchAmmount == 3:
-            print("Player ",pplayer,"WINS!")
+            print("Player ",pplayer,"WINS!")  # Insert Win here
             return True
 
         rowMatchAmmount = 0
         collumMatchAmmount = 0
         i=0
         r+=1
-
+    # We are checking the diagnols this time
     diagnalOneMatch = 0
     diagnalTwoMatch = 0
     y=0
     z=2
     while y<3:
         if m[y][y] == lletter:
-            diagnalOneMatch += 1
+            diagnalOneMatch += 1  # Diagnol count
         if m[y][z] == lletter:
-            diagnalTwoMatch += 1
+            diagnalTwoMatch += 1 # Other diagnol count
         y+=1
         z-=1
 
     if diagnalOneMatch == 3:
-        print("Player ",pplayer,"WINS!")
+        print("Player ",pplayer,"WINS!")  # Win Prompt
         return True
     if diagnalTwoMatch == 3:
-        print("Player ",pplayer,"WINS!")
+        print("Player ",pplayer,"WINS!")  # Other win Promt
         return True
     return False
 
-
+#Setting up the matrix, both the virtual game and its positioning
 m = [[' ', ' ', ' '], [' ', ' ', ' '], [' ', ' ', ' ']]
 gameMap = [["Top Left", "Top Middle", "Top Right"], ["Middle Left", "Middle Middle", "Middle Right"],
            ["Bottom Left", "Bottom Middle", "Bottom Right"]]
@@ -106,10 +107,10 @@ player = 1
 playerLetter = 'X'
 gameOver = False
 while gameOver == False:
-    printTikTakTow(m)
-    m = playerCommand(player, m, gameMap, playerLetter)
-    gameOver = checkConnectThree(m, playerLetter, player)
-    if player == 1:
+    printTikTakTow(m) #Print virtual game
+    m = playerCommand(player, m, gameMap, playerLetter) # update map
+    gameOver = checkConnectThree(m, playerLetter, player) # check victory condition
+    if player == 1:  # swap player and letter
         player = 2
         playerLetter = 'O'
     else:
